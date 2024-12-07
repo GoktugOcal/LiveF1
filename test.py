4a55198281b6453f298807473988a38d55d8b222
goktugocal <goktugocal41@gmail.com>
2024-12-07 14:58:18 +0100

    topic test

--- test.py
+++ test.py
@@ -14,7 +14,7 @@
 
 import livef1
 
-session = livef1.get_meeting(season=2024, meeting_identifier="Spa")
+# session = livef1.get_meeting(season=2024, meeting_identifier="Spa")
 
 # print(session.meeting_table)
 
@@ -27,3 +27,30 @@ session = livef1.get_meeting(season=2024, meeting_identifier="Spa")
 
 # print(season.meetings_table)
 # print(season.season_table)
+
+# session = livef1.get_session(season=2024, meeting_identifier="Spa", session_identifier="Race")
+# print(session.name)
+
+# from livef1.utils.constants import session_index
+
+# print(list(session_index["Feeds"].keys()))
+
+import json
+from livef1.adapters.livetimingf1_adapter import livetimingF1_request
+
+seasons = [
+    2018,
+    2019,
+    2020,
+    2021,
+    2022,
+    2023,
+    2024
+]
+
+
+sess = livef1.get_season(2021).meetings[0].sessions[0]
+
+print(sess)
+# sess.get_topic_names()
+# print(sess.topic_names_info)
