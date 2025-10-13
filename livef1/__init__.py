633de28e229b6b76c5e5f4fa8e89f95c251a959d
goktugocal <goktugocal41@gmail.com>
2025-10-13 15:45:37 +0200

    added source information to car telemetry table as "tag"

--- livef1/__init__.py
+++ livef1/__init__.py
@@ -23,7 +23,7 @@ from pandas.errors import SettingWithCopyWarning
 warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-__version__ = "1.0.93"
+__version__ = "1.0.94"
 
 __all__ = [
     'set_log_level',
