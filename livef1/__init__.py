e871b79da29588c688a7190823c4be51ae8269ad
goktugocal <goktugocal41@gmail.com>
2025-11-07 20:26:40 +0100

    version update

--- livef1/__init__.py
+++ livef1/__init__.py
@@ -23,7 +23,7 @@ from pandas.errors import SettingWithCopyWarning
 warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-__version__ = "1.0.94"
+__version__ = "1.0.95"
 
 __all__ = [
     'set_log_level',
