422c206b39bbad132f3120222249fc8a9d84db2f
goktugocal <goktugocal41@gmail.com>
2026-02-18 14:36:13 +0000

    uvloop removed because of the windows incompability

--- livef1/__init__.py
+++ livef1/__init__.py
@@ -27,7 +27,7 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 # warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
 # warnings.simplefilter(action='ignore', category=FutureWarning)
 
-__version__ = "1.1.100"
+__version__ = "1.1.101"
 
 __all__ = [
     'set_log_level',
